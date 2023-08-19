9bc08d9a11a1117d12c96ec6ff29d1b79f715b43
Devanshu Jain <jaindevanshu237@gmail.com>
2023-08-19 21:23:57 +0530

    New Change - 20

--- file12.js
+++ file12.js
@@ -1,8 +1,12 @@
 // 1. call, apply, bind 
+// 2. arrow functions 
+// 3. Object creation using Object.create()
+// 4. proto, prototype, class
+// 5. A good practice to create, multiple objects using same properties and methods
 
 
 
-// __________________________________________________
+// _______________________________________________________________
 // call
 //  - parentObject.callingFunction.call(childObject) 
 // - child object determines the 'this' of parent object
@@ -11,6 +15,9 @@ const user1 = {
     age : 22,
     about : function(){
         console.log(this.fName, this.age);
+    },
+    hobby : function(instrument, singer){
+        console.log(`${instrument} and ${singer}`);
     }
 }
 
@@ -21,3 +28,120 @@ const user2 = {
 
 user1.about();
 user1.about.call(user2);  //user2 borrows from user1
+user1.about.call();  // undefined undefined
+user1.hobby.call(user2, "Guitar", "Shawn Mendes");
+
+
+// _______________________________________________________________
+// apply : same as call
+user1.about();
+user1.about.apply(user2);
+user1.hobby.call(user2, ["Guitar", "Shawn Mendes"]); // in case of call, array of elems is considered as 1.
+user1.hobby.apply(user2, ["Guitar", "Shawn Mendes"]);
+
+
+// _______________________________________________________________
+// bind : returns a function
+function about(hobby, favMusician){
+    console.log(this.fName, this.age, hobby, favMusician);
+}
+
+const user3 = {
+    fName : "Jethalal", 
+    age : 50,
+}
+
+const user4 = {
+    fName : "Daya", 
+    age : 45,
+}
+
+const func1 = about.bind(user3, "guitar", "Shawn Musician");
+func1();
+const func2 = about.bind(user4, "guitar", "Shawn Musician");
+func2();
+
+
+// _______________________________________________________________
+const u1 = {
+    fName : "Devanshu",
+    age : 22,
+    about : function(){
+        console.log(this.fName, this.age);
+    }
+}
+
+const myFunc = u1.about;
+myFunc(); // undefined undefined
+
+// why?
+const myFunc1 = function(){
+    console.log(this.fName, this.age);  //here 'this' is window object
+};
+myFunc1(); // functionCaller is window object
+
+const myFunc2 = u1.about.bind(u1);
+myFunc2(); // Devanshu 22
+
+
+
+/// _______________________________________________________________
+// arrow functions : arrow function ka "this" nahi hota
+// arrow function takes "this" from its parent elem. object's "this" will be window's "this"
+
+
+// _______________________________________________________________
+// Object creation using Object.create() : returns an empty array
+
+const objPar = {
+    key1 : "Value1",
+    key2 : "Value2",
+    key3 : "Value45"
+}
+
+const objCh1 = Object.create(objPar); // creates an empty object, but has Parent methods in it
+objCh1["key3"] = "Value3";
+objCh1["key4"] = "Value4";
+
+console.log(objCh1.key4);
+
+
+// __proto__   &   [[prototype]]
+//      - Both are same thing
+//      - __proto__ is the parent object, from where methods and properties will be inherited by the child object
+
+console.log(objCh1);
+console.log(objCh1.__proto__);
+
+
+// _______________________________________________________________
+// A good practice to create, multiple objects using same properties and methods.
+
+const displayInfo = {
+    is50(){
+        return "Above 50";
+    },
+
+    displayFullName(){
+        return this.fName +" "+ this.lName;
+    }
+}
+
+function userDetails(fName, lName, email, age){
+    const user = Object.create(displayInfo);  //Inner field cannot be left empty
+    user["fName"] = fName;
+    user["lName"] = lName;
+    user["email"] = email;
+    user["age"] = age;
+    return user;
+}
+
+const student1 = userDetails("Jethalal", "Gada", "jg@gmail", "50");
+const student2 = userDetails("Taarak", "Mehta", "tm@gmail", "54");
+const student3 = userDetails("Roshan", "Sodhi", "rs@gmail", "45");
+const student4 = userDetails("Hansraj", "Hathi", "hh@gmail", "57");
+
+console.log(student1);
+console.log(student1.displayFullName());
+console.log(student2);
+console.log(student2.displayFullName());
