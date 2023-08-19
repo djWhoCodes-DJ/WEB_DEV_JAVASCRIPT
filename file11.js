a550087e572265d9b0b99795b1474dbd2c34bec2
Devanshu Jain <jaindevanshu237@gmail.com>
2023-08-19 12:53:56 +0530

    New Change - 18

--- file11.js
+++ file11.js
@@ -3,6 +3,10 @@
 //     - has method
 //     - for of loop
 // 3. Maps
+// 4. cloning object
+//    - Object.assign()
+// 5. optional chaining
+// 6. methods
 
 
 
@@ -106,3 +110,85 @@ for (let [key, value] of person){
 
 const employee = new Map([[1, "Devanshu"], [2, "Rahul"]]);
 console.log(employee);
+
+
+// __________________________________________________
+//cloning object
+// objects are reference type, i.e. they store the addresses
+const obj1 = {
+    Name : "Devanshu",
+    age : 22
+}
+
+const obj2 = obj1;
+
+obj1['gender'] = 'male';
+
+console.log(obj1);
+console.log(obj2);
+
+
+//cloning
+const obj3 = {...obj1};
+obj1["hobby"] = "guitar";
+
+console.log(obj1);
+console.log(obj3);
+
+
+//cloning using Object.assign
+const obj4 = Object.assign({}, obj1);
+
+obj1["sleepyhead"] = "yes";
+
+console.log(obj1);
+console.log(obj4);
+
+
+// __________________________________________________
+//optional chaining
+
+const user = {
+    Id : 5,
+    Name : "Devanshu",
+    // hobby : {instrument : "guitar"}
+}
+
+console.log(user.Id);
+console.log(user.Name);
+console.log(user.hobby);
+// console.log(user.hobby.instrument) //to remove this error, if hobby does not exist now, but will emerge later
+console.log(user?.hobby?.instrument) // '?' checks whether it exists, if not return undefined. So error does not occur and code runs
+
+
+// __________________________________________________
+// methods: functions inside object
+// "this" keyword : "this" is caller object, calling out the function
+
+const person1 = {
+    fName : "Devanshu",
+    age : 22,
+    about: function(){
+        // console.log(`person name is ${fName} and age is ${age}`) // error
+        console.log(`person name is ${this.fName} and age is ${this.age}`) // error
+        console.log(this);
+    }
+}
+
+person1.about();
+
+
+// __________________________________________________
+console.log(window);
+console.log(this);
+console.log(window===this);
+
+function greet(){
+    "use strict" // will return undefined from below line, if called without caller object
+    console.log(this);
+}
+
+window.greet();
+
+
+// __________________________________________________
